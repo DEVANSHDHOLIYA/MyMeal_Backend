941b99e69a6f2f24553dd02d3d404c8cecb73525
Devansh Dholiya <devanshdholiya2568@gmail.com>
2026-04-02 20:31:09 +0530

    first commit

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ import express from 'express';
 import { FRONTEND_URL, PORT } from './src/config/config.js';
 import { connectDB } from './src/Db/database.js';
 
-import cors from 'cors';
 import { indexRoute } from './src/routes/index.routes.js';
 const app= express();
 
-app.use(cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      FRONTEND_URL
-    ],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  }))
+
 app.use(express.json());
 app.use('/api/v1',indexRoute);
 connectDB().then(()=>{
